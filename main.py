b52848dfbfa9cfaa8d0e9490fc55ae44f4e3dc17
foxeditor <65810020+montelisa@users.noreply.github.com>
2020-06-15 22:49:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
-
+#!coding: UTF-8
+# Import modules
 # Import modules
 from requests import get
 from bs4 import BeautifulSoup
 from colorama import Fore
 from prettytable import PrettyTable
-
+import urllib, json, os
+init(convert=True)
 # Request headers
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; ) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4086.0 Safari/537.36",
@@ -16,17 +16,30 @@ headers = {
 }
 
 print(rf"""{Fore.LIGHTGREEN_EX}
-  _____                                        _       ____                  _                  
- |_   _|   ___    _ __   _ __    ___   _ __   | |_    / ___|    ___    ___  | | __   ___   _ __ 
-   | |    / _ \  | '__| | '__|  / _ \ | '_ \  | __|   \___ \   / _ \  / _ \ | |/ /  / _ \ | '__|
-   | |   | (_) | | |    | |    |  __/ | | | | | |_     ___) | |  __/ |  __/ |   <  |  __/ | |   
-   |_|    \___/  |_|    |_|     \___| |_| |_|  \__|   |____/   \___|  \___| |_|\_\  \___| |_|   
-                                                                           {Fore.LIGHTYELLOW_EX} > Created by LimerBoy
+ _____                          _       ____           _                        
+|__   __|                      | |    / ____|         | |            
+   | | ___  _ __ _ __ ___ _ __ | |_  | (___   ___  ___| | _____ _ __ 
+   | |/ _ \| '__| '__/ _ \ '_ \| __|  \___ \ / _ \/ _ \ |/ / _ \ '__|
+   | | (_) | |  | | |  __/ | | | |_   ____) |  __/  __/   <  __/ |   
+   |_|\___/|_|  |_|  \___|_| |_|\__| |_____/ \___|\___|_|\_\___|_|    
+                                    {Fore.LIGHTYELLOW_EX} > Created by LimerBoy
 {Fore.RESET}
 """)
 
 # Get user input
-ip = input(f" {Fore.LIGHTCYAN_EX}TorrentSeeker@target_ip : {Fore.LIGHTMAGENTA_EX}")
+init(convert=True)
+ip = input('TorыыrentSeeker@target_ip: ' )
+
+
+#IP Info
+url = "https://ipinfo.io/" + ip + "/json"
+info = urllib.request.urlopen( url )
+ls = json.load(info)
+print( "City -> ", ls["city"])
+print("Region -> ",ls["region"])
+print( "Location -> ", ls["loc"] )
+print( "Operator -> ", ls["org"])
+print("\n")
 
 # Get request
 page = get("https://iknowwhatyoudownload.com/en/peer/?ip=" + ip,
